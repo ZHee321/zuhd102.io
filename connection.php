09e1d5e74dd1827a86958aeeffab6f7dcfcba4b9
Zuh02 <maspuzuhdi07@student.hamzanwadi.ac.id>
2024-07-15 09:03:28 +0800

    first commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$dbhost = "	sql204.infinityfree.com";
+$dbusername = "if0_36890005";
+$dbpassword = "Anak5olEh";
+$dbname = "if0_36890005_adminPHP";
+
+$connection = mysqli_connect($dbhost, $dbusername, $dbpassword,  $dbname);
+?>
---
+++ helper/connection.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "phpcrud_uas";
+
+$connection = mysqli_connect($dbhost, $dbusername, $dbpassword,  $dbname);
